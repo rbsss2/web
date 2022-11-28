190d51c0a77acafdc37b8e383e242e1eb27c46c2
rbsss2 <rbsss2@naver.com>
2022-11-28 17:59:05 +0900

    파추

---
+++ 220919-js/연산자Ex.js
@@ -0,0 +1,13 @@
+// 연산자 연습
+let num1 = 12 % 5;// 2
+num1 = 10 % 5;// 0
+console.log(num1);
+console.log(1 % 5);// 1
+console.log(4 % 4);// 0
+console.log(6 % 7);// 6
+console.log(0 % 3);// 0
+console.log(4 % 3 + 1);// 4 % 3 -> 1 + 1 -> 2
+console.log(5 + 6 % 4);// 6 % 4 -> 2, 5 + 2 -> 7
+console.log((10 + 2) % 3);// 10 + 2 -> 12 % 3 -> 0
+console.log(5 * (16 % 4));// 16 % 4 -> 0 * 5 -> 0
+console.log((9 % 8) + 3 ** 2);// 9 % 8 -> 1, 3 * 3 -> 9, 1 + 9 -> 10
